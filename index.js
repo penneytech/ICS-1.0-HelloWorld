a5ba625a138edfe108701bbd63ee0d3cb8cc6bea
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-05 20:26:11 -0400

    Updated with new workflow.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Log "Hello World" to the console.
 // Log four additional messages of your choosing. 
 // Comment each message appropriately. 
-console.log("Testing!");
+
+console.log("Here is a console log example!");
